22b9157755a688612a18e09bd6efc3e2f28de67f
pedrovgs <pedrovicente.gomez@gmail.com>
2015-03-04 01:39:12 +0100

    Create Nox repository

---
+++ sample/src/main/java/com/github/pedrovgs/nox/sample/MainActivity.java
@@ -0,0 +1,12 @@
+package com.github.pedrovgs.nox.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
