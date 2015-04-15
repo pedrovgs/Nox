4086402144095bd19567f10ccf7c1b3704dd7f13
pedrovgs <pedrovicente.gomez@gmail.com>
2015-04-15 19:22:04 +0200

    Add CircularTransformation to glide

---
+++ nox/src/main/java/com/github/pedrovgs/nox/transformation/CircularTransformation.java
@@ -0,0 +1,48 @@
+package com.github.pedrovgs.nox.transformation;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
+public class CircularTransformation extends BitmapTransformation {
+  private Context context;
+
+  public CircularTransformation(Context context) {
+    super(context);
+    this.context = context;
+  }
+
+  @Override
+  protected Bitmap transform(BitmapPool pool, Bitmap source, int outWidth, int outHeight) {
+    return getCircularBitmapImage(source);
+  }
+
+  @Override public String getId() {
+    return "Glide_Circle_Transformation";
+  }
+
+  public static Bitmap getCircularBitmapImage(Bitmap source) {
+    int size = Math.min(source.getWidth(), source.getHeight());
+    int x = (source.getWidth() - size) / 2;
+    int y = (source.getHeight() - size) / 2;
+    Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+    if (squaredBitmap != source) {
+      source.recycle();
+    }
+    Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    Paint paint = new Paint();
+    BitmapShader shader =
+        new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+    paint.setShader(shader);
+    paint.setAntiAlias(true);
+    float r = size / 2f;
+    canvas.drawCircle(r, r, r, paint);
+    squaredBitmap.recycle();
+    return bitmap;
+  }
+}
