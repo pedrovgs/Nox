7e0f019b45a80c1c7464ad7fa266a66362a9b62d
pedrovgs <pedrovicente.gomez@gmail.com>
2015-05-01 19:06:46 +0200

    Improve AppsActivity to show a list of installed apps

--- sample/src/main/java/com/github/pedrovgs/nox/sample/AppsActivity.java
+++ sample/src/main/java/com/github/pedrovgs/nox/sample/AppsActivity.java
@@ -16,16 +16,48 @@
 
 package com.github.pedrovgs.nox.sample;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.github.pedrovgs.nox.NoxItem;
+import com.github.pedrovgs.nox.NoxView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppsActivity extends ActionBarActivity {
+
+  private NoxView noxView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_apps);
+    initializeNoxView();
+  }
+
+  private void initializeNoxView() {
+    noxView = (NoxView) findViewById(R.id.nox_view);
+    List<NoxItem> apps = getApps();
+    noxView.showNoxItems(apps);
+  }
+
+  private List<NoxItem> getApps() {
+    List<NoxItem> apps = new ArrayList<NoxItem>();
+    for (ApplicationInfo app : getInstalledApps()) {
+      String packageName = app.packageName;
+      if (app.icon != 0) {
+        Uri uri = Uri.parse("android.resource://" + packageName + "/" + app.icon);
+        apps.add(new NoxItem(uri.toString()));
+      }
+    }
+    return apps;
+  }
+
+  private List<ApplicationInfo> getInstalledApps() {
+    return getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA);
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
